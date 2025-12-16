e8fdb3ff4c85294248f9c78a2bc8834cf32e7929
alirajabzadeh <geked25573@dakcans.com>
2025-12-15 18:14:06 -0800

    Create Driver Projects

---
+++ OakDriver/OakDriver/OakDriver.cpp
@@ -0,0 +1,10 @@
+#include <ntddk.h>
+
+
+extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+
+	UNREFERENCED_PARAMETER(DriverObject);
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	return STATUS_SUCCESS;
+}
